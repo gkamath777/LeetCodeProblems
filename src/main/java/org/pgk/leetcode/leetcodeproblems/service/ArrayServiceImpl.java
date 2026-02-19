3a595dbd0d702f578dc9b75464a6c5fd7ad2caf8
Gaurav Kamath <gkamath3@illinois.edu>
2026-02-19 08:46:06 -0800

    Added solution to the #45 Jump game II - Added the service method

--- src/main/java/org/pgk/leetcode/leetcodeproblems/service/ArrayServiceImpl.java
+++ src/main/java/org/pgk/leetcode/leetcodeproblems/service/ArrayServiceImpl.java
@@ -465,4 +465,27 @@ public class ArrayServiceImpl {
         }
     }
 
+
+    public int jumpGame2(int[] nums) {
+        int farthest = 0;
+        int end = 0;
+        int jumps = 0;
+        int n = nums.length;
+        if(n==1)
+            return 0;
+
+        for (int i =0; i< n;i++) {
+            farthest = Math.max(farthest, i+ nums[i]);
+            if(i == end) {
+                jumps++;
+                end = farthest;
+
+                if(end >= n-1)
+                    break;
+            }
+        }
+
+        return jumps;
+    }
+
 }
